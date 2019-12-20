79219bee95fc5e23b68b076f2a1337016cede5b1
Katsuki Kobayashi <rare@tirasweel.org>
2019-12-20 19:43:35 +0900

    add keyword to headline

--- tests/orgdata_test.py
+++ tests/orgdata_test.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import unittest
 
 from orgorganizer.node import parse_lines
@@ -9,18 +10,19 @@ class NodeTest(unittest.TestCase):
     def test_headline_1level(self):
         """レベル1のHEADLINEテスト"""
 
-        document = parse_lines("* [#A] hoge  :a:b:")
+        document = parse_lines("*  TODO [#A] hoge  :a:b:")
         it = document.__iter__()
 
         node = next(it)
         self.assertEqual(node.level, 1)
         self.assertEqual(node._parsed, False)
+        self.assertEqual(node.keyword, "TODO")
         self.assertEqual(node.title, "hoge")
         self.assertEqual(node._parsed, True)
         self.assertEqual(node.tags, ["a", "b"])
         self.assertEqual(node.priority, "A")
         self.assertEqual(
-            node.__str__(), "* [#A] hoge" + " " * 61 + ":a:b:",
+            node.__str__(), "* TODO [#A] hoge" + " " * 56 + ":a:b:",
         )
 
         with self.assertRaises(StopIteration):
@@ -29,14 +31,15 @@ class NodeTest(unittest.TestCase):
     def test_headline_2level(self):
         """レベル2のHEADLINEテスト"""
 
-        document = parse_lines("** fuga")
+        document = parse_lines("** DONE fuga")
         it = document.__iter__()
 
         node = next(it)
         self.assertEqual(node.level, 2)
         self.assertEqual(node.title, "fuga")
         self.assertEqual(node._parsed, True)
-        self.assertEqual(node.__str__(), "** fuga")
+        self.assertEqual(node.keyword, "DONE")
+        self.assertEqual(node.__str__(), "** DONE fuga")
 
         with self.assertRaises(StopIteration):
             next(it)
@@ -46,8 +49,8 @@ class NodeTest(unittest.TestCase):
 
         document = parse_lines(
             """* hoge
-** fuga
-** moge
+** TODO fuga
+** DONE moge
 *** mogemoge
 *** mogemoge
 """
@@ -63,19 +66,22 @@ class NodeTest(unittest.TestCase):
         node2 = next(it)
         self.assertEqual(node2.level, 2)
         self.assertEqual(node2.title, "fuga")
-        self.assertEqual(node2.__str__(), "** fuga")
         self.assertEqual(node2._parsed, True)
+        self.assertEqual(node2.keyword, "TODO")
+        self.assertEqual(node2.__str__(), "** TODO fuga")
 
         node3 = next(it)
         self.assertEqual(node3.level, 2)
         self.assertEqual(node3.title, "moge")
-        self.assertEqual(node3.__str__(), "** moge")
+
+        self.assertIn(node2, node1.children)
+        self.assertIn(node3, node1.children)
         self.assertEqual(node3._parsed, True)
+        self.assertEqual(node3.keyword, "DONE")
+        self.assertEqual(node3.__str__(), "** DONE moge")
 
         self.assertEqual(node2.parent, node1)
         self.assertEqual(node3.parent, node1)
-        self.assertIn(node2, node1.children)
-        self.assertIn(node3, node1.children)
 
         node4 = next(it)
         self.assertEqual(node4.level, 3)
@@ -97,3 +103,12 @@ class NodeTest(unittest.TestCase):
 
         with self.assertRaises(StopIteration):
             next(it)
+
+        self.assertEqual(
+            document.__str__(),
+            """* hoge
+** TODO fuga
+** DONE moge
+*** mogemoge
+*** mogemoge""",
+        )
